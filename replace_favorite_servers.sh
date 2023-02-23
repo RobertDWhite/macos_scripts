bd43698644efda7d29d180998424f8dc28edd994
Robert <robert@whitematter.tech>
2023-02-23 01:17:06 -0500

    update to make this better

--- replace_favorite_servers.sh
+++ replace_favorite_servers.sh
@@ -5,10 +5,11 @@
 ## download the .sfl2 file from your storage location ##
 # for Google Drive, replace the ID at the end of the URL
 # For another storage location, replace the full URL
-curl -L -o "com.apple.LSSharedFileList.FavoriteServers.sfl2"  "https://drive.google.com/uc?export=download&id=PUT_FILE_ID_HERE"
+curl -L -o /tmp/com.apple.LSSharedFileList.FavoriteServers.sfl2  "https://drive.google.com/uc?export=download&id=PUT_FILE_ID_HERE"
 
-# copy the file you just downloaded to the proper location
-cp com.apple.LSSharedFileList.FavoriteServers.sfl2 ~/Library/Application\ Support/com.apple.sharedfilelist/
+# move sfl2 to correct directory
+for i in $(ls /Users | grep -v "^_"); do mkdir -p /Users/$i/Library/Application\ Support/com.apple.sharedfilelist/ && cp -f /tmp/com.apple.LSSharedFileList.FavoriteServers.sfl2 /Users/$i/Library/Application\ Support/com.apple.sharedfilelist/ && chmod 775 /Users/$i/Library/Application\ Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.FavoriteServers.sfl2;done
 
 # reboot for changes to take effect
 reboot
+
